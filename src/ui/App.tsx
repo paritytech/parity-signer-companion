4f5fe96cf318ed1042a2d8f6a2e4858747eec0fb
Andrei Eres <eresav@me.com>
2021-09-13 12:47:16 +0300

    Extract layout

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,14 +1,14 @@
 import { View } from '@polkadot/extension-ui/components'
 import '@polkadot/extension-ui/i18n/i18n'
-import Popup from '@polkadot/extension-ui/Popup'
 import React, { Suspense } from 'react'
 import { HashRouter } from 'react-router-dom'
+import Layout from './Layout'
 
 export const App: React.FC = () => (
   <Suspense fallback='...'>
     <View>
       <HashRouter>
-        <Popup />
+        <Layout />
       </HashRouter>
     </View>
   </Suspense>
