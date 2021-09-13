fc3a11a1ee81c16b4ca443d58537f407dbaa0290
Andrei Eres <eresav@me.com>
2021-09-13 09:44:15 +0300

    fix

--- webpack.watch.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const createConfig = require('./webpack.shared.js')
-
-module.exports = createConfig({
-  background: './src/background.ts',
-  content: './src/content.ts',
-  extension: './src/extension.ts',
-  page: './src/page.ts',
-})
