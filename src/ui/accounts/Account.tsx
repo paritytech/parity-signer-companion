52a9cb1e002bc8bf8c8804d2dd80a26fa4c022e4
Andrei Eres <eresav@me.com>
2021-09-14 13:55:53 +0300

    Export Account

---
+++ src/ui/accounts/Account.tsx
@@ -0,0 +1,87 @@
+import type { AccountJson } from '@polkadot/extension-base/background/types'
+import { Address, Link } from '@polkadot/extension-ui/components'
+import useTranslation from '@polkadot/extension-ui/hooks/useTranslation'
+import React from 'react'
+import styled from 'styled-components'
+import { ThemeProps } from '../types'
+
+interface Props extends AccountJson {
+  className?: string
+  parentName?: string
+}
+
+const Account: React.FC<Props> = ({
+  address,
+  className,
+  genesisHash,
+  isExternal,
+  isHidden,
+  name,
+  parentName,
+  suri,
+}) => {
+  const { t } = useTranslation()
+
+  return (
+    <div className={className}>
+      <Address
+        actions={
+          <Link className='menuItem' isDanger to={`/account/forget/${address}`}>
+            {t('Forget Account')}
+          </Link>
+        }
+        address={address}
+        className='address'
+        genesisHash={genesisHash}
+        isExternal={isExternal}
+        isHidden={isHidden}
+        name={name}
+        parentName={parentName}
+        suri={suri}
+      />
+    </div>
+  )
+}
+
+export default styled(Account)`
+  .address {
+    margin-bottom: 8px;
+  }
+
+  .editName {
+    position: absolute;
+    flex: 1;
+    left: 70px;
+    top: 10px;
+    width: 350px;
+
+    .danger {
+      background-color: ${({ theme }: ThemeProps) => theme.bodyColor};
+      margin-top: -13px;
+      width: 330px;
+    }
+
+    input {
+      height: 30px;
+      width: 350px;
+    }
+
+    &.withParent {
+      top: 16px;
+    }
+  }
+
+  .menuItem {
+    border-radius: 8px;
+    display: block;
+    font-size: 15px;
+    line-height: 20px;
+    margin: 0;
+    min-width: 13rem;
+    padding: 4px 16px;
+
+    .genesisSelection {
+      margin: 0;
+    }
+  }
+`
---
+++ src/ui/accounts/AccountsTree.tsx
@@ -0,0 +1,24 @@
+import type { AccountWithChildren } from '@polkadot/extension-base/background/types'
+import React from 'react'
+import Account from './Account'
+
+interface Props extends AccountWithChildren {
+  parentName?: string
+}
+
+const AccountsTree: React.FC<Props> = ({ parentName, suri, ...account }) => {
+  return (
+    <>
+      <Account {...account} parentName={parentName} suri={suri} />
+      {account?.children?.map((child, index) => (
+        <AccountsTree
+          key={`${index}:${child.address}`}
+          {...child}
+          parentName={account.name}
+        />
+      ))}
+    </>
+  )
+}
+
+export default AccountsTree
