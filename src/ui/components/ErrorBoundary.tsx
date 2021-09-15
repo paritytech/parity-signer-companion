48d4282615d571140b68eab981b34c6f2fd99a01
Andrei Eres <eresav@me.com>
2021-09-15 08:57:16 +0300

    Remove t from ErrorBoundary

--- src/ui/components/ErrorBoundary.tsx
+++ src/ui/components/ErrorBoundary.tsx
@@ -1,10 +1,8 @@
-import translate from '@polkadot/extension-ui/components/translate'
 import React from 'react'
-import { WithTranslation } from 'react-i18next'
 import { goTo } from '../utils/goTo'
 import Header from './Header'
 
-type Props = WithTranslation
+type Props = {}
 
 type State = {
   error?: Error
@@ -22,21 +20,15 @@ class ErrorBoundary extends React.Component<Props, State> {
   }
 
   public override render() {
-    const { children, t } = this.props
-    const { error } = this.state
-
-    if (!error) return children
+    if (!this.state.error) return this.props.children
 
     return (
       <>
-        <Header text={t('An error occured')} />
+        <Header text={'An error occured'} />
         <div>
-          {t(
-            'Something went wrong with the query and rendering of this component. {{message}}',
-            { replace: { message: error.message } }
-          )}
+          {`Something went wrong with the query and rendering of this component. ${this.state.error.message}`}
         </div>
-        <button onClick={this.goHome}>{t('Back to home')}</button>
+        <button onClick={this.goHome}>{'Back to home'}</button>
       </>
     )
   }
@@ -46,4 +38,4 @@ class ErrorBoundary extends React.Component<Props, State> {
   }
 }
 
-export default translate(ErrorBoundary)
+export default ErrorBoundary
