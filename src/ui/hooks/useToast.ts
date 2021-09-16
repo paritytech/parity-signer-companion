45447b33f34c57dc1c15ef1b163de20e237b8223
Andrei Eres <eresav@me.com>
2021-09-16 08:58:10 +0300

    Rip toasts (#19)

--- src/ui/hooks/useToast.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { ToastContext } from '../contexts'
-
-export default function useToast(): { show: (message: string) => void } {
-  return useContext(ToastContext)
-}
