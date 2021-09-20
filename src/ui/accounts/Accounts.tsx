244760b9cd69ebe019c942b01d30c91515eebe21
Andrei Eres <eresav@me.com>
2021-09-20 19:40:32 +0300

    Update accounts

--- src/ui/accounts/Accounts.tsx
+++ src/ui/accounts/Accounts.tsx
@@ -1,9 +1,9 @@
 import { useStore } from 'nanostores/react'
 import React from 'react'
 import styled from 'styled-components'
+import Key from '../components/Key'
 import { hierarchy as hierarchyStore } from '../stores/accounts'
 import { BaseProps } from '../types'
-import AccountsTree from './AccountsTree'
 import NoAccounts from './NoAccounts'
 
 const Accounts: React.FC<BaseProps> = ({ className }) => {
@@ -14,8 +14,13 @@ const Accounts: React.FC<BaseProps> = ({ className }) => {
   return (
     <div className={className}>
       <h1>All keys</h1>
-      {hierarchy.map((json, i) => (
-        <AccountsTree {...json} key={`${i}:${json.address}`} />
+      {hierarchy.map((account) => (
+        <Key
+          address={account.address}
+          genesisHash={account.genesisHash}
+          name={account.name}
+          key={account.address}
+        />
       ))}
     </div>
   )
