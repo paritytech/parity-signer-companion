63565f8ccce598aed6279cb938be1811967043b2
Andrei Eres <eresav@me.com>
2021-09-20 17:42:24 +0300

    Update Address

--- src/ui/components/Address.tsx
+++ src/ui/components/Address.tsx
@@ -12,6 +12,10 @@ import { findAccountByAddress } from '../utils/findAccountByAddress'
 import { recodeAddress, Recoded } from '../utils/recodeAddress'
 import { goTo } from '../utils/routing'
 import Identicon from '@polkadot/react-identicon'
+import cancelIcon from '../assets/cancel.svg'
+import copyIcon from '../assets/copy.svg'
+import { useTimedReset } from '../hooks/useTimedReset'
+import { Chain } from '@polkadot/extension-chains/types'
 
 type Props = BaseProps & {
   address?: string
@@ -30,13 +34,13 @@ const defaultRecoded = {
 
 const Address: React.FC<Props> = ({
   address,
-  children,
   className,
   genesisHash,
   name,
   type: givenType,
   hideActions,
 }) => {
+  const [justCopied, setJustCopied] = useTimedReset<boolean>(false)
   const [recoded, setRecoded] = useState<Recoded>(defaultRecoded)
   const accounts = useStore(accountsStore) as AccountJson[]
   const chain = useMetadata(genesisHash || recoded.genesisHash, true)
@@ -47,9 +51,17 @@ const Address: React.FC<Props> = ({
       ? 'ethereum'
       : 'polkadot'
   ) as IconTheme
+  const nameLabel = name || recoded.account?.name || '<unknown>'
+  const hashLabel =
+    (justCopied && 'Copied') || recoded.formatted || address || '<unknown>'
 
   const onCopy = () => {
-    navigator.clipboard.writeText(recoded.formatted || '').catch(console.error)
+    if (justCopied) return
+
+    navigator.clipboard
+      .writeText(hashLabel)
+      .then(() => setJustCopied(true))
+      .catch(console.error)
   }
   const forget = () => goTo(`/account/forget/${address}`)
 
@@ -74,45 +86,95 @@ const Address: React.FC<Props> = ({
 
   return (
     <div className={className}>
-      <Identicon
-        prefix={recoded.prefix}
-        theme={iconTheme}
-        value={recoded.formatted || address}
-      />
-      <div>
-        <div>{name || recoded.account?.name || '<unknown>'}</div>
-        <div className='address'>
-          {recoded.formatted || address || '<unknown>'}
-        </div>
-        {!hideActions && (
-          <div className='actions'>
-            <button onClick={onCopy}>{'Copy'}</button>
-            <button onClick={forget}>{'Forget'}</button>
-          </div>
-        )}
+      <div className='logo'>
+        <Identicon
+          prefix={recoded.prefix}
+          theme={iconTheme}
+          value={recoded.formatted || address}
+          size={50}
+        />
       </div>
-      {children}
+      <div className='content'>
+        <div className='name'>{nameLabel}</div>
+        <div
+          className={`address highlighted ${justCopied && 'just-copied'}`}
+          onClick={onCopy}
+        >
+          <div className='icon copy'>
+            <img src={copyIcon} />
+          </div>
+          <div className='hash'>{hashLabel}</div>
+        </div>
+      </div>
+      {!hideActions && (
+        <div className='icon cancel highlighted' onClick={forget}>
+          <img src={cancelIcon} />
+        </div>
+      )}
     </div>
   )
 }
 
 export default styled(Address)`
-  width: 100%;
+  display: flex;
+  position: relative;
   background: ${({ theme }: Props) => theme.cardBgColor};
-  margin-bottom: 0.5rem;
   border-radius: 0.2rem;
-  padding: 0.4rem;
-  padding-top: 0.2rem;
+
+  .logo {
+    padding: 0.5rem;
+  }
+
+  .logo svg {
+    cursor: default;
+  }
+
+  .content {
+    padding: 0.5rem 0;
+    padding-right: 1rem;
+  }
+
+  .name {
+    margin-bottom: 0.2rem;
+  }
 
   .address {
+    display: flex;
+    align-items: center;
+    font-size: ${({ theme }: Props) => theme.smallFontSize};
     color: ${({ theme }: Props) => theme.fadedTextColor};
   }
 
-  .actions {
-    margin-top: 0.4rem;
+  .hash {
+    padding: 0 0.2rem;
   }
 
-  .actions * {
-    margin-right: 0.4rem;
+  .icon {
+    width: 1rem;
+    height: 1rem;
+  }
+
+  .cancel {
+    position: absolute;
+    top: 0.2rem;
+    right: 0.2rem;
+  }
+
+  .highlighted {
+    border-radius: 0.2rem;
+    transition: ${({ theme }: Props) => theme.transition};
+    cursor: pointer;
+  }
+
+  .highlighted:hover {
+    background: ${({ theme }: Props) => theme.hightlight};
+  }
+
+  .highlighted.just-copied {
+    background: none;
+  }
+
+  & + & {
+    margin-top: 0.2rem;
   }
 `
