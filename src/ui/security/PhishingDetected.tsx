ad379bedd8a36cfb1f532d022ae7c2120f4f1be9
Andrei Eres <eresav@me.com>
2021-09-21 09:16:24 +0300

    Update phishing page

--- src/ui/security/PhishingDetected.tsx
+++ src/ui/security/PhishingDetected.tsx
@@ -9,24 +9,31 @@ const PhishingDetected: React.FC<BaseProps> = ({ className }) => {
   const decodedWebsite = decodeURIComponent(website)
 
   return (
-    <>
-      <div className={className}>
+    <div className={className}>
+      <h1>Phishing detected</h1>
+      <p>
         You have been redirected because the extension believes that this
-        website could compromise the security of your accounts and your tokens.
-        <div>{decodedWebsite}</div>
-        <div>
-          Note that this website was reported on a community-driven, curated
-          list. It might be incomplete or inaccurate. If you think that this
-          website was flagged incorrectly,{' '}
-          <a href='https://github.com/polkadot-js/phishing/issues/new'>
-            please open an issue by clicking here
-          </a>
-        </div>
-      </div>
-    </>
+        website could compromise the security of your accounts and your tokens:
+      </p>
+      <p className='website'>{decodedWebsite}</p>
+      <p className='note'>
+        Note that this website was reported on a community-driven, curated list.
+        It might be incomplete or inaccurate. If you think that this website was
+        flagged incorrectly,{' '}
+        <a href='https://github.com/polkadot-js/phishing/issues/new'>
+          please open an issue by clicking here
+        </a>
+      </p>
+    </div>
   )
 }
 
 export default styled(PhishingDetected)`
-  height: 100%;
+  .website {
+    text-align: center;
+  }
+
+  .note {
+    margin-top: 2rem;
+  }
 `
