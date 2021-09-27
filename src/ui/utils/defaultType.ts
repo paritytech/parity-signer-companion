7f2ee98b27da9f8c2dabd846c60d499817eb5b7f
Andrei Eres <eresav@me.com>
2021-09-27 15:16:42 +0300

    Move DEFAULT_TYPE

--- src/ui/utils/defaultType.ts
+++
@@ -1,3 +0,0 @@
-import { KeypairType } from '@polkadot/util-crypto/types'
-
-export const DEFAULT_TYPE = 'sr25519' as KeypairType
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
+import { KeypairType } from '@polkadot/util-crypto/types'
+
 export const PKG_NAME = process.env.PKG_NAME ?? ''
 export const PKG_VERSION = process.env.PKG_VERSION ?? ''
 export const ROOT_ID = 'root'
 export const EXT_NAME = 'Parity Signer Companion'
 export const UNKNOWN = '<unknown>'
 export const RELAY_CHAIN = 'Relay Chain'
+export const DEFAULT_TYPE = 'sr25519' as KeypairType
