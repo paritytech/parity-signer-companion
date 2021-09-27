ab30e820aa240b3937791bc8be47eb5565479f59
Andrei Eres <eresav@me.com>
2021-09-27 15:26:25 +0300

    Move findSubstrateAccount

--- src/utils/recodeAddress.ts
+++ src/utils/recodeAddress.ts
@@ -5,7 +5,7 @@ import { SettingsStruct } from '@polkadot/ui-settings/types'
 import { decodeAddress, encodeAddress } from '@polkadot/util-crypto'
 import { KeypairType } from '@polkadot/util-crypto/types'
 import { DEFAULT_TYPE } from 'src/utils/constants'
-import { findSubstrateAccount } from '../ui/utils/findSubstrateAccount'
+import { findSubstrateAccount } from './findSubstrateAccount'
 import { startSettings } from './startSettings'
 
 export interface Recoded {
