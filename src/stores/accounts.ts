0a49d6fcc8831cee73732694f847b542b9432e7b
Andrei Eres <eresav@me.com>
2021-09-27 16:10:35 +0300

    Update accounts store

--- src/stores/accounts.ts
+++ src/stores/accounts.ts
@@ -1,5 +1,4 @@
 import { AccountJson } from '@polkadot/extension-base/background/types'
-import { canDerive } from '@polkadot/extension-base/utils'
 import { createDerived, createStore } from 'nanostores'
 import { buildHierarchy } from '../utils/buildHierarchy'
 import { subscribeAccounts } from '../utils/messaging'
@@ -11,14 +10,6 @@ export const accounts = createStore<AccountJson[]>(() => {
 
 export const hasAccounts = createDerived(accounts, (list) => list.length > 0)
 
-export const hierarchy = createDerived(accounts, (list) =>
-  buildHierarchy(list as AccountJson[])
-)
-
-export const masterAccount = createDerived(hierarchy, (list) =>
-  list.find(({ isExternal, type }) => !isExternal && canDerive(type))
-)
-
 export const setAccounts = (list: AccountJson[]) => {
-  accounts.set(list)
+  accounts.set(buildHierarchy(list))
 }
