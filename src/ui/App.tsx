79f77c81f59505a43ce71427026c058298951c7a
Andrei Eres <eresav@me.com>
2021-09-27 15:35:33 +0300

    Simplify App

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,25 +1,20 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { ThemeProvider } from 'styled-components'
+import { goHome } from '../utils/routing'
 import ErrorFallback from './components/ErrorFallback'
-import Loading from './components/Loading'
 import Main from './components/Main'
 import { GlobalStyle } from './GlobalStyle'
 import Router from './Router'
 import { theme } from './themes'
-import { goHome } from '../utils/routing'
 
 export const App: React.FC = () => (
-  <Suspense fallback={'...'}>
-    <Loading>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle theme={theme} />
-        <Main>
-          <ErrorBoundary FallbackComponent={ErrorFallback} onReset={goHome}>
-            <Router />
-          </ErrorBoundary>
-        </Main>
-      </ThemeProvider>
-    </Loading>
-  </Suspense>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle theme={theme} />
+    <Main>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={goHome}>
+        <Router />
+      </ErrorBoundary>
+    </Main>
+  </ThemeProvider>
 )
