17c0dd1d92ae32d71902091f248db00a4c5c1f39
Andrei Eres <eresav@me.com>
2021-10-04 16:49:48 +0300

    Add editable name (#51)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,7 +10,7 @@ export const PKG_NAME = process.env.PKG_NAME ?? ''
 export const PKG_VERSION = process.env.PKG_VERSION ?? ''
 export const ROOT_ID = 'root'
 export const EXT_NAME = 'Parity Signer Companion'
-export const UNKNOWN = '<unknown>'
+export const UNKNOWN = 'Unknown'
 export const RELAY_CHAIN = 'Relay Chain'
 export const DEFAULT_TYPE = 'sr25519' as KeypairType
 
