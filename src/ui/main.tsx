26a6758f3787db6fd9e40a7e707e3146246b0c62
Andrei Eres <eresav@me.com>
2022-04-20 15:49:02 +0200

    Update react (#107)
    * Update React
    
    * Update react mounting
    
    * Allow scripts

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { ROOT_ID } from '../utils/constants'
 import { App } from './App'
 
-const rootElement = document.getElementById(ROOT_ID)
-if (!rootElement) throw new Error(`Unable to find element with id '${ROOT_ID}'`)
+const container = document.getElementById(ROOT_ID)
+if (!container) throw new Error(`Unable to find element with id '${ROOT_ID}'`)
 
-ReactDOM.render(<App />, rootElement)
+const root = createRoot(container)
+root.render(<App />)
