95f49f442adf205d025231608e8b2a2b4af6d0f2
Andrei Eres <eresav@me.com>
2022-04-21 10:20:15 +0200

    Add support for postcss

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { goHome } from '../utils/routing'
 import ErrorFallback from './components/ErrorFallback'
-import { GlobalStyle } from './global/GlobalStyle'
 import Main from './global/Main'
 import Router from './global/Router'
+import './index.css'
 
 export const App: React.FC = () => (
   <Main>
-    <GlobalStyle />
     <ErrorBoundary FallbackComponent={ErrorFallback} onReset={goHome}>
       <Router />
     </ErrorBoundary>
