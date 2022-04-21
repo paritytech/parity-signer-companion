f7b7c6cb8d07766973aa1c112e3aae6384cb722b
Andrei Eres <eresav@me.com>
2022-04-21 17:40:32 +0200

    Refactor routing

--- src/ui/global/Router.tsx
+++ src/ui/global/Router.tsx
@@ -1,32 +1,26 @@
-import { PHISHING_PAGE_REDIRECT } from '@polkadot/extension-base/defaults'
 import { useStore } from '@nanostores/react'
 import React from 'react'
+import { routeStore } from '../../stores/router'
 import Accounts from '../accounts/Accounts'
 import ImportQr from '../accounts/ImportQr'
 import Authorize from '../requests/Authorize'
 import Metadata from '../requests/Metadata'
 import Signing from '../requests/Signing'
 import PhishingDetected from '../security/PhishingDetected'
-import { authRequestsStore } from '../../stores/authRequests'
-import { metaRequestsStore } from '../../stores/metaRequests'
-import { router } from '../../stores/router'
-import { signRequestsStore } from '../../stores/signRequests'
-import { exact, exactWithParam } from '../../utils/routing'
 
 const Router: React.FC = () => {
-  const { path } = useStore(router)
-  const authRequests = useStore(authRequestsStore)
-  const metaRequests = useStore(metaRequestsStore)
-  const signRequests = useStore(signRequestsStore)
+  const route = useStore(routeStore)
 
-  if (exact(path, '') && authRequests?.length) return <Authorize />
-  if (exact(path, '') && metaRequests?.length) return <Metadata />
-  if (exact(path, '') && signRequests?.length) return <Signing />
-  if (exact(path, '/account/import')) return <ImportQr />
-  if (exactWithParam(path, `${PHISHING_PAGE_REDIRECT}/:website`))
-    return <PhishingDetected />
-
-  return <Accounts />
+  return (
+    <>
+      {route === 'auth' && <Authorize />}
+      {route === 'meta' && <Metadata />}
+      {route === 'sign' && <Signing />}
+      {route === 'import' && <ImportQr />}
+      {route === 'phishing' && <PhishingDetected />}
+      {route === 'accounts' && <Accounts />}
+    </>
+  )
 }
 
 export default Router
