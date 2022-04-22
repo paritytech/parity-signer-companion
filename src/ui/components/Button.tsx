7352404c7ed92ca3ae97f80c845e3a09ef9b3566
Andrei Eres <eresav@me.com>
2022-04-22 15:56:39 +0200

    Update keys appearance (#118)

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -4,15 +4,18 @@ import { cn } from '../../utils/cn'
 export const Button = ({
   className,
   isSecondary,
+  isDanger,
   ...rest
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & {
   isSecondary?: boolean
+  isDanger?: boolean
 }) => (
   <button
     className={cn(
       'inline-block rounded px-8 py-1 whitespace-nowrap',
       !isSecondary && 'bg-_action-400 text-_action-600',
       isSecondary && 'bg-_bg-300 text-_text-300 ',
+      isDanger && 'bg-_bg-danger text-_signal-danger',
       className
     )}
     {...rest}
